804c4f8d2b61ef6f9a9113baa3d988fbc710eee9
phucanh14797 <phucanh14797@gmail.com>
2021-10-17 02:51:56 +0900

    Please submit assignment 1

---
+++ SQL/Assignment 1/Testing_System_Assignment_1.sql
@@ -0,0 +1,89 @@
+DROP DATABASE IF EXISTS testing_system_assignment_1;
+CREATE DATABASE testing_system_assignment_1;
+USE testing_system_assignment_1;
+
+-- Department
+CREATE TABLE Department ( 
+	DepartmentID  		INT UNSIGNED PRIMARY KEY,
+    DepartmentName    	VARCHAR(30) UNIQUE KEY
+); 
+-- Poisition
+CREATE TABLE `Position` (
+	PositionID		TINYINT UNSIGNED PRIMARY KEY,
+    PositionName	ENUM('Dev', 'Test', 'Scrum Master', 'PM')
+);
+-- Account
+CREATE TABLE `Account` (
+	AccountID		INT UNSIGNED PRIMARY KEY,
+    Email			VARCHAR(100) UNIQUE KEY,
+    Username		VARCHAR(100) UNIQUE KEY,
+    FullName		VARCHAR(100),
+    DepartmentID	INT UNSIGNED,
+    PositionID		TINYINT UNSIGNED,
+    CreateDate		DATE,
+    FOREIGN KEY(DepartmentID) REFERENCES Department(DepartmentID),
+    FOREIGN KEY(PositionID) REFERENCES `Position`(PositionID)
+);
+-- Group
+CREATE TABLE `Group` (
+	GroupID 		INT UNSIGNED PRIMARY KEY,
+    GroupName		VARCHAR(100),
+    CreatorID		INT UNSIGNED,
+    CreateDate		DATE,
+    FOREIGN KEY(CreatorID) REFERENCES `Account`(AccountID)
+);
+-- GroupAccount
+CREATE TABLE GroupAccount (
+	GroupID			INT UNSIGNED,
+	AccountID		INT UNSIGNED,	
+	JoinDate 		DATE,
+    FOREIGN KEY(GroupID) REFERENCES `Group`(GroupID),
+    FOREIGN KEY(AccountID) REFERENCES `Account`(AccountID)
+);
+-- Type Question
+CREATE TABLE TypeQuestion (
+	TypeID			INT UNSIGNED PRIMARY KEY,
+	TypeName		ENUM('Essay','Multiple-Choise')
+);
+-- CategoryQuestion
+CREATE TABLE CategoryQuestion (
+	CategoryID			INT UNSIGNED PRIMARY KEY,
+	CategoryName		VARCHAR(30)
+);
+-- Question
+CREATE TABLE Question (
+	QuestionID			INT UNSIGNED PRIMARY KEY,
+	Content				VARCHAR(100),
+    CategoryID			INT UNSIGNED,
+    TypeID				INT UNSIGNED,
+	CreatorID			INT UNSIGNED,
+    CreateDate			DATE,
+    FOREIGN KEY(CreatorID) REFERENCES `Account`(AccountID)
+);
+-- Answer
+CREATE TABLE Answer (
+	AnswerID			INT UNSIGNED PRIMARY KEY,
+	Content				VARCHAR(100),
+    QuestionID			INT UNSIGNED,
+    isCorrect			ENUM('Correct','NOT Correct'),
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID)
+);
+-- Exam
+CREATE TABLE Exam (
+	ExamID				INT UNSIGNED PRIMARY KEY,
+	`Code`				INT UNSIGNED,
+	Title				VARCHAR(100),
+    CategoryID			INT UNSIGNED,
+    Duration			TIME,
+    CreatorID			INT UNSIGNED,
+    CreateDate			DATE,
+    FOREIGN KEY(CreatorID) REFERENCES `Account`(AccountID),
+    FOREIGN KEY(CategoryID) REFERENCES CategoryQuestion(CategoryID)
+);
+-- ExamQuestion
+CREATE TABLE ExamQuestion (
+	ExamID				INT UNSIGNED,
+	QuestionID			INT UNSIGNED,
+    FOREIGN KEY(ExamID) REFERENCES Exam(ExamID),
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID)
+);
