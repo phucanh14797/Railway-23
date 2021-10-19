750ce97fc5ba5e9a358d46ddb2fffde41d9fa9a2
phucanh14797 <phucanh14797@gmail.com>
2021-10-20 00:57:29 +0900

    updated. please check

--- SQL/Assignment 2/Testing_System_Assignment_3_QUESTION.sql
+++ SQL/Assignment 2/Testing_System_Assignment_3_QUESTION.sql
@@ -7,7 +7,19 @@ FROM department;
 -- QUESTION 3: LẤY RA ID PHÒNG BAN "SALE"
 SELECT department.department_id
 FROM department
-WHERE department_name = 'sale'
+WHERE department_name = 'sale';
 
 -- QUESTION 4: LẤY RA THÔNG TIN ACCOUNT CÓ FULLNAME DÀI NHẤT
+SELECT *
+FROM `account` 
+WHERE LENGTH(fullname) = (
+	SELECT MAX(LENGTH(fullname))
+    FROM `account`);
+
 -- QUESTION 5: LẤY RA THÔNG TIN ACCOUNT CÓ FULLNAME DÀI NHẤT & ID=3
+SELECT *
+FROM `account` 
+WHERE LENGTH(fullname) = (
+	SELECT MAX(LENGTH(fullname))
+    FROM `account`) 
+AND account_id = 3;
