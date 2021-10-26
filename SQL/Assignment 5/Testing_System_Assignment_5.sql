90911af2ffb281e9705a89fa1a52901b6ae8b9a9
phucanh14797 <phucanh14797@gmail.com>
2021-10-27 01:50:35 +0900

    Please CHECK !

--- SQL/Assignment 5/Testing_System_Assignment_5.sql
+++ SQL/Assignment 5/Testing_System_Assignment_5.sql
@@ -3,8 +3,13 @@ USE `testing_system_assignment_3`;
 
 CREATE VIEW v_nhan_vien_phong_sales AS(
 			SELECT ac.fullname
-            FROM `account` ac
-            WHERE department_id = 2
+            FROM `account` ac JOIN department d ON ac.department_id = d.department_id
+            WHERE d.department_name = (
+					SELECT d.department_name
+					FROM department d
+					WHERE d.department_id = 2
+					)
+		
 );
 
 -- Question 2: Tạo view có chứa thông tin các account tham gia vào nhiều group nhất
@@ -16,7 +21,7 @@ CREATE VIEW v_account_tham_gia_nhieu_group AS(
 			SELECT ac.fullname, COUNT(ga.group_id)
             FROM `account` ac JOIN group_account ga ON ac.account_id = ga.account_id
             GROUP BY ac.account_id HAVING COUNT(ga.group_id) = (
-											SELECT MAX(tim_max)
+								 			SELECT MAX(tim_max)
 											FROM ( 
 													SELECT COUNT(ga.group_id) AS tim_max
 													FROM group_account ga JOIN `account` ac ON ac.account_id = ga.account_id
@@ -25,13 +30,13 @@ CREATE VIEW v_account_tham_gia_nhieu_group AS(
 
 -- Question 3: Tạo view có chứa câu hỏi có những content quá dài (content quá 300 từ được coi là quá dài) và xóa nó đi
 
-DROP VIEW IF EXISTS v_cau_hoi_dai;
+DROP VIEW IF EXISTS v_cau_hoi_dai_qua_300;
 CREATE VIEW v_cau_hoi_dai_qua_300 AS(
-			SELECT q.content qc
+			SELECT q.content
             FROM question q 
-            WHERE count_words > 300
+            WHERE LENGTH(content) - LENGTH(REPLACE(content, ' ', '')) + 1 > 300
 );
-DELETE FROM qc ;
+DELETE FROM v_cau_hoi_dai_qua_300;
 
 -- Question 4: Tạo view có chứa danh sách các phòng ban có nhiều nhân viên nhất
 
@@ -51,8 +56,12 @@ CREATE VIEW v_danh_sach_phong_ban_nhieu_nv AS(
 
 DROP VIEW IF EXISTS v_cau_hoi_user_ho_Nguyen;
 CREATE VIEW v_cau_hoi_user_ho_Nguyen AS (
-			SELECT q.content
+			SELECT q.content, ac.fullname
             FROM question q JOIN `account` ac ON ac.account_id = q.creator_id
-            WHERE ac.fullname LIKE 'NGUYEN%'
-            
+            WHERE ac.fullname IN (
+				SELECT ac.fullname
+                FROM `account`
+                WHERE ac.fullname LIKE 'NGUYEN%'
+                )
 );
+SELECT * FROM v_cau_hoi_user_ho_Nguyen;
